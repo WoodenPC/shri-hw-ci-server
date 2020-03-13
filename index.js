ab2ad6c27d1f6fb098f7da54414afd9ecf4d51e1
Arslanov Rasul <arslanovrasul@MacBook-Pro.local>
2020-03-13 23:14:41 +0300

    added static files

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.use(require('./routes'));
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static(path.resolve(__dirname, 'views/public')));
 
 const SERVER_PORT = 3000;
 
